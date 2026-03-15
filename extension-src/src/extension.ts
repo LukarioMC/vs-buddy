1aa48354477a381d03db9900b4acd0a84e0dabd1
Dakaro Mueller <LukarioMC@gmail.com>
2026-03-14 18:48:54 -0700

    Added basic display of WASM Unity app

--- extension-src/src/extension.ts
+++ extension-src/src/extension.ts
@@ -41,7 +41,12 @@ class GameViewProvider implements vscode.WebviewViewProvider {
 
 	private _getHtmlForWebview(webview: vscode.Webview) {
 		const nonce = getNonce();
-		// This is where you will eventually link your Unity index.html
+		const buildPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'unity-build');
+		const loaderUri = webview.asWebviewUri(vscode.Uri.joinPath(buildPath, 'Build.loader.js'));
+		const frameworkUri = webview.asWebviewUri(vscode.Uri.joinPath(buildPath, 'Build.framework.js'));
+		const dataUri = webview.asWebviewUri(vscode.Uri.joinPath(buildPath, 'Build.data'));
+		const wasmUri = webview.asWebviewUri(vscode.Uri.joinPath(buildPath, 'Build.wasm'));
+
 		return `<!DOCTYPE html>
             <html lang="en">
 			<head>
@@ -51,15 +56,47 @@ class GameViewProvider implements vscode.WebviewViewProvider {
 					and only allow scripts that have a specific nonce.
 					(See the 'webview-sample' extension sample for img-src content security policy examples)
 				-->
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src ${webview.cspSource} 'nonce-${nonce}' 'unsafe-eval'; connect-src ${webview.cspSource};">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<title>VS Buddy</title>
+				<style>
+                    html, body {
+						padding: 0;
+						margin: 0;
+						width: 100%;
+						height: 100%;
+						overflow: hidden;
+						display: flex;
+					}
+					#unity-container {
+						width: 100% !important;
+						height: 100% !important;
+						position: absolute;
+					}
+					#unity-canvas {
+						width: 100% !important;
+						height: 100% !important;
+						display: block;
+					}
+                </style>
 			</head>
             <body>
-                <h1>Game Loading...</h1>
+                <div id="unity-container" class="unity-desktop">
+                    <canvas id="unity-canvas" tabindex="-1"></canvas>
+                </div>
+                <script nonce="${nonce}" src="${loaderUri}"></script>
                 <script nonce="${nonce}">
                     const vscode = acquireVsCodeApi();
-                    // Unity loader logic goes here
+                    createUnityInstance(document.querySelector("#unity-canvas"), {
+                        dataUrl: "${dataUri}",
+                        frameworkUrl: "${frameworkUri}",
+                        codeUrl: "${wasmUri}",
+                        streamingAssetsUrl: "StreamingAssets",
+                        companyName: "DefaultCompany",
+                        productName: "VS Buddy",
+                        productVersion: "0.1",
+						devicePixelRatio: 1,
+                    });
                 </script>
             </body>
             </html>`;
@@ -74,4 +111,3 @@ function getNonce() {
 	}
 	return text;
 }
-
